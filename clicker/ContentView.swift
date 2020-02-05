1cc929f572d301ce96bff15207cf1584b974d997
Erich Shan <ershan@umich.edu>
2020-02-05 14:28:54 -0500

    Initial Commit

---
+++ clicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clicker
+//
+//  Created by Erich Shan on 2/5/20.
+//  Copyright © 2020 Erich Shan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
