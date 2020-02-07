c9380411ed30646baf7ae766c1e2b82db1a21002
Erich Shan <ershan@umich.edu>
2020-02-06 19:21:48 -0500

    first commit

--- clicker/ContentView.swift
+++ clicker/ContentView.swift
@@ -8,9 +8,40 @@
 
 import SwiftUI
 
+class Casper : Identifiable {
+    
+    var id: Int
+    
+    init(id: Int){
+        self.id = id
+    }
+    
+}
+
+
+
+
+
 struct ContentView: View {
+    var howHeDied : [String] = ["On the toilet", "EECS OH queue", "Eaten by core leads", "Junlin", "Onion eating contest", "Vinegar shot", "Jeff bezos death stare", "Soylent", "Ragequit", "Sad boi hours"]
+    
+    @State var caspers : [Casper] = []
+    @State var label = 69
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Spacer()
+            Text("Number of Caspers dead:" + String(label))
+            Spacer(minLength: CGFloat(20.0))
+            Text(String("Cause of death:"))
+            Text(howHeDied[Int.random(in: 1..<howHeDied.count)])
+            Spacer(minLength: CGFloat(20.0))
+            Button(action: {
+                self.caspers.append(Casper(id: self.label))
+                self.label+=1 }) {
+                Text(String("kill a casper"))
+            }
+            Spacer()
+        }
     }
 }
 
