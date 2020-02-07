7564bc8167012da584f91fea0d6214424f6f028d
Erich Shan <ershan@umich.edu>
2020-02-06 19:37:33 -0500

    refactored

--- clicker/ContentView.swift
+++ clicker/ContentView.swift
@@ -22,21 +22,30 @@ class Casper : Identifiable {
 
 
 
+
 struct ContentView: View {
-    var howHeDied : [String] = ["On the toilet", "EECS OH queue", "Eaten by core leads", "Junlin", "Onion eating contest", "Vinegar shot", "Jeff bezos death stare", "Soylent", "Ragequit", "Sad boi hours"]
+    func nonRepeatRandom() -> Int {
+        var newVal = 0
+        repeat {
+            newVal = Int.random(in: 1..<self.howHeDied.count)
+        }while(newVal == self.randInt)
+        return newVal
+    }
+    var howHeDied : [String] = ["On the toilet", "EECS OH queue", "Eaten by core leads", "Junlin", "Onion eating contest", "Vinegar shot", "Jeff bezos death stare", "Soylent", "Ragequit", "Sad boi hours", "too many bugs", "infinite loop", "segmentation fault", "non-agile practices", "yeeted"]
     
     @State var caspers : [Casper] = []
     @State var label = 69
+    @State var randInt = 0
     var body: some View {
         VStack{
             Spacer()
             Text("Number of Caspers dead:" + String(label))
             Spacer(minLength: CGFloat(20.0))
             Text(String("Cause of death:"))
-            Text(howHeDied[Int.random(in: 1..<howHeDied.count)])
+            Text(howHeDied[self.randInt])
             Spacer(minLength: CGFloat(20.0))
             Button(action: {
-                self.caspers.append(Casper(id: self.label))
+                self.randInt = self.nonRepeatRandom()
                 self.label+=1 }) {
                 Text(String("kill a casper"))
             }
@@ -44,6 +53,8 @@ struct ContentView: View {
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
